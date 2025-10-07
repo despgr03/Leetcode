375d12d3d07e14aca39688a0619c4eb6aac4b15a
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-10-07 16:08:30 +0300

    Update 1.TwoSum.java

--- Easy/1.TwoSum.java
+++ Easy/1.TwoSum.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[] {i,j};
-                }
+        Map<Integer, Integer> complement = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (complement.containsKey(nums[i])) {
+                return new int[] { complement.get(nums[i]), i };
             }
+            complement.put(target - nums[i], i);
         }
-        return  new int[] {};
+        return new int[] {};
     }
 }
