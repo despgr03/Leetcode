e7ab323501a38aa4d1351a24945a42630b36634c
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-10-07 17:54:54 +0300

    Update 242. Valid Anagram.java

--- Easy/242. Valid Anagram.java
+++ Easy/242. Valid Anagram.java
@@ -2,17 +2,9 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int s1 = s.length();
         int t1 = t.length();
-        if(s1!=t1){
-            return false;
-        }
-        while (t1 > 0 && s1 > 0) {
+        while (t1 > 0 && s1 > 0 && s1==t1) {
             t = t.replace(String.valueOf(s.charAt(0)), "");
             s = s.replace(String.valueOf(s.charAt(0)), "");
-            if (t.length() == 0 && s.length() == 0 && t1 - t.length() == s1 - s.length()) {
-                return true;
-            } else if (t.length() == t1 || t1 - t.length() != s1 - s.length()) {
-                return false;
-            }
             s1 = s.length();
             t1 = t.length();
         }
